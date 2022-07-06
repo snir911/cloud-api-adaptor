2c2e8f525c8d6dba9d90d82ad35f3ad82639ab62
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2022-07-06 11:50:59 +0000

    Add support for AWS AMI creation using packer

---
+++ aws/image/copy-files.sh
@@ -0,0 +1,8 @@
+sudo mkdir -p /etc/containers
+sudo cp -r /tmp/files/etc/containers/* /etc/containers/
+sudo cp -r /tmp/files/etc/systemd/* /etc/systemd/
+
+
+sudo mkdir -p /usr/local/bin
+sudo cp -r /tmp/files/usr/* /usr/
+
